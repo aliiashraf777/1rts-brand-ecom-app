e02369d8cc2147c2703ae5d3b296be86abca1225
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-06-30 03:19:20 +0500

    feat(singleProduct): updaate layout

--- src/pages/shop/SingleProductDetail.tsx
+++ src/pages/shop/SingleProductDetail.tsx
@@ -4,7 +4,10 @@ import { DynamicBreadCrumb } from "@/components/shop/BreadCrumb";
 import StarsDynamicRatings from "@/components/shop/products/StarsDynamicRatings";
 import { productDetailsData, productsData, suppliersData } from "@/data/productsData"
 import { useCartActions } from "@/redux/features/cart/useCartActions";
+import { selectFavsItemsList } from "@/redux/features/favs/favsSelectors";
 import { useFavsActions } from "@/redux/features/favs/useFavsActions";
+import { useAppSelector } from "@/redux/features/storeHooks";
+import type { IProductItem } from "@/types/productTypes";
 import { Check, Globe, HeartIcon, MessageSquareText, ShieldCheck, ShoppingBasket } from "lucide-react";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router"
@@ -46,6 +49,9 @@ const SingleProductDetail = (props: Props) => {
 
   const { addToCart } = useCartActions();
   const { addToFavs } = useFavsActions();
+  const favsItemsList = useAppSelector(selectFavsItemsList);
+
+  const isAddedToFavs: IProductItem | undefined = favsItemsList.find((i) => i.id === id);
 
   return (<>
     <DynamicBreadCrumb />
@@ -92,7 +98,7 @@ const SingleProductDetail = (props: Props) => {
           </div>
 
           {/* details wrapper ------------------- */}
-          <div className="xl:w-[430px] flex flex-1 shrink-0 flex-col gap-2 borderx border-gray-300 rounded-card">
+          <div className="xl:w-[430px] flex flex-1 shrink-0 flex-col gap-2 borderx border-gray-300 rounded-card mb-5">
 
             {/* title & review */}
             <p className="hidden md:flex items-center gap-1 txt-body-shrik text-brand-green">
@@ -143,13 +149,13 @@ const SingleProductDetail = (props: Props) => {
               </div>
 
               <div className="flex flex-col gap-1 pr-2.5 md:pr-10">
-                <h3 className="heading-h5">$98.00</h3>
-                <p className="txt-small text-gray-800">50-100 pcs</p>
+                <h3 className="heading-h5">$90.00</h3>
+                <p className="txt-small text-gray-800">100-700 pcs</p>
               </div>
 
               <div className="flex flex-col gap-1">
-                <h3 className="heading-h5">$98.00</h3>
-                <p className="txt-small text-gray-800">50-100 pcs</p>
+                <h3 className="heading-h5">$78.00</h3>
+                <p className="txt-small text-gray-800">700+ pcs</p>
               </div>
             </div>
 
@@ -161,44 +167,43 @@ const SingleProductDetail = (props: Props) => {
                   className="flex flex-col gap-1 md:gap-4"
                 >
 
-                  <div className="flex gap-[98px]">
-                    <span className="text-gray-500">Price: </span>
-                    <span className="text-gray-600">{detail.price}</span>
-                  </div>
-
-                  <div className="w-full h-px bg-gray-300"></div>
-
-                  <div className="flex gap-[98px]">
-                    <span className="text-gray-500">Type: </span>
-                    <span className="text-gray-600">{detail.type}</span>
-                  </div>
-
-                  <div className="flex gap-[98px]">
-                    <span className="text-gray-500">Material: </span>
-                    <span className="text-gray-600">{detail.material}</span>
-                  </div>
-
-                  <div className="flex gap-[98px]">
-                    <span className="text-gray-500">Design: </span>
-                    <span className="text-gray-600">{detail.design}</span>
-                  </div>
-
-                  <div className="w-full h-px bg-gray-300"></div>
-
-                  <div className="flex gap-[98px]">
+                  {/* <div className="flex gap-[98px]">
                     <span className="text-gray-500">Customization: </span>
                     <span className="text-gray-600">{detail.customization}</span>
-                  </div>
+                  </div> */}
 
-                  <div className="flex gap-[98px]">
-                    <span className="text-gray-500">Protection: </span>
-                    <span className="text-gray-600">{detail.protection}</span>
-                  </div>
+                  {[
+                    { label: 'Price', value: detail.price },
+                    { label: 'Type', value: detail.type },
+                    { label: 'Material', value: detail.material },
+                    { label: 'Design', value: detail.design },
+                  ].map(({ label, value }, idx) => (
+                    <div key={idx}>
+                      {idx === 1 &&
+                        <div className="w-full h-px bg-gray-300 mb-4" />}
 
-                  <div className="flex gap-[98px]">
-                    <span className="text-gray-500">Warranty: </span>
-                    <span className="text-gray-600">{detail.warranty}</span>
-                  </div>
+                      <div className="grid grid-cols-[140px_1fr] py-1.5x">
+                        <span className="text-gray-500">{label}</span>
+                        <span className="text-gray-600">{value}</span>
+                      </div>
+                    </div>
+                  ))}
+
+                  <div className="w-full h-px bg-gray-300" />
+
+                  {[
+                    { label: 'Customization', value: detail.customization },
+                    { label: 'Protection', value: detail.protection },
+                    { label: 'Warranty', value: detail.warranty },
+                  ].map(({ label, value }, idx) => (
+                    <div
+                      key={idx}
+                      className="grid grid-cols-[140px_1fr]"
+                    >
+                      <span className="text-gray-500">{label}</span>
+                      <span className="text-gray-600">{value}</span>
+                    </div>
+                  ))}
 
                   <div className="w-full h-px bg-gray-300"></div>
 
@@ -279,15 +284,27 @@ const SingleProductDetail = (props: Props) => {
 
             {/* addToWishlist btn */}
             <div className="w-full grid place-items-center">
-              <BorderReorderBtn
-                className="txt-body-medium! text-primary! text-center! justify-center! w-max! h-max! border-transparent! hover:bg-transparent hover:border-primary!"
-                onClick={() => addToFavs({
-                  id, title, image, price
-                })}
-              >
-                <HeartIcon />
-                <span>Save for later</span>
-              </BorderReorderBtn>
+              {!isAddedToFavs
+                ? (<>
+                  <BorderReorderBtn
+                    className="txt-body-medium! text-primary! text-center! justify-center! w-max! h-max! border-transparent! hover:bg-transparent hover:border-primary!"
+                    onClick={() => addToFavs({
+                      id, title, image, price
+                    })}
+                  >
+                    <HeartIcon />
+                    <span>Save for later</span>
+                  </BorderReorderBtn>
+                </>)
+                : (<>
+                  <BorderReorderBtn
+                    className="txt-body-medium! text-brand-green! text-center! justify-center! w-max! h-max! border-transparent! hover:bg-transparent hover:border-brand-green!"
+                  >
+                    <Check />
+                    <span>Saved for later</span>
+                  </BorderReorderBtn>
+                </>)
+              }
             </div>
 
           </div>
